f4e6e526c670a7a01a9c01dac11376285c179d26
abir835 <abirhasan.raj.bd@gmail.com>
2023-08-19 13:06:00 +0600

    user registration done

--- account/views.py
+++ account/views.py
@@ -1,8 +1,13 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from account.serializers import UserRegistrationSerializer
 
 
 class UserRegistrationView(APIView):
-    def get(self, request, format=None):
-        return Response({'msg': 'Registration Success'})
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'mag': 'Registration Success'}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
